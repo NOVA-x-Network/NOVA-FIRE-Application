3e401c1bdd42207543ed76f0f6ea3ffa3a6bec5d
Stephen Le <stephensgamingguide@gmail.com>
2021-07-21 21:02:06 -0400

    Added mobile-friendly index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,149 +1,83 @@
 import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
-
+import nova_logo_cropped from "../images/cropped-NOVA-logo-1.png"
+import nova_front_art from "../images/NovaHomepageClipArt.png"
 // markup
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+	let width = window.innerWidth
+	console.log(width)
+	var introTextWidth
+	var introTextFont
+	var frontBannerWidth
+	var frontBannerHeight
+	var frontBannerImageWidth
+	var frontBannerImageHeight
+	var cornerImageHeight
+	var cornerImageWidth
+	if (width >= 1025) {
+		introTextFont = "2vh"
+		introTextWidth = "510px"
+		frontBannerHeight = "40vh"
+		frontBannerImageHeight = "30vh"
+		frontBannerWidth = "60vw"
+		frontBannerImageWidth = "22vw"
+		cornerImageHeight = "8vh"
+		cornerImageWidth="16vh"
+	}
+	else {
+		introTextFont = "1.5vh"
+		introTextWidth="180px"
+		frontBannerHeight = "20vh"
+		frontBannerImageHeight = "10vh"
+		frontBannerWidth = "90vw"
+		frontBannerImageWidth = "25w"
+		cornerImageHeight = "4vh"
+		cornerImageWidth="8vh"
+    }
+	console.log(introTextWidth)
+	return (<div>
+				<div style={{display:"flex", flex:1}}>
+					<img src={nova_logo_cropped} style={{ height: cornerImageHeight, width: cornerImageWidth, marginLeft:"8px", marginTop:"8px" }} />
+				</div>
+				<div style={{ display: "flex", flex: 9, flexDirection: "column", alignItems:"center", marginTop:"12px"}}>
+					<div style={{ background: "linear-gradient(90deg, rgba(17,8,153,1) 0%, rgba(25,180,103,1) 97%)", display:"flex",justifyContent:"left", width:frontBannerWidth, borderRadius:"20px", height:frontBannerHeight, alignItems:"center"}}>
+				<div style={{ display: "flex", flexDirection: "column", marginLeft: "25px",}}>
+					<p style={{ fontFamily: "arial", lineHeight: `1.4`, color: "white", fontSize: introTextFont, textAlign: "left", width: introTextWidth}}>
+						NOVA FIRE (Fellowship in Innovation, Research, and Education) is a
+						5 month-long intensive fellowship program for high school students.
+						Our fellows will participate in primary and secondary interdisciplinary
+						research in 1 of our 4 topics. They will be given the opportunity to either
+						develop a hands-on project, or a written report for publication, based on their findings
+						and analysis. Students will have a chance to work in teams of 5-6.
+						NOVA will be accepting a maximum of 24 students into our program.
+				</p>
+					<form method="get" action="https://novaxnetwork.com/fellowship/">
+						<button type="submit">Learn more</button>
+					</form>
+				</div>
+				<div style={{ display: "flex", justifyContent: "center", flex:"auto"}}>
+					<img src={nova_front_art} style={{ width: frontBannerImageWidth, height: frontBannerImageHeight, marginRight: "0px" }} />
+				</div>
+					</div>
+					<div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+						<p style={{ fontSize: "6vh", fontWeight: "bold", fontFamily: "arial", marginBottom:"15px"}}>
+								NOVA Fellowship Applications are now open!
+						</p>
+						<br />
+						<div style={{display:"flex", flexDirection:"row"}}>
+							<div className="gradient-button-border" onClick={() => { window.location = "./signup" }}>
+								<div>
+									<p style={{ color: "#07024e", fontFamily: "arial", fontWeight: "bold" }}>Apply Now!</p>
+								</div>
+							</div>
+							<div className="gradient-button-border" onClick={() => { window.location = "./login" }}>
+								<div >
+									<p style={{ color: "#07024e", fontFamily: "arial", fontWeight: "bold" }}>Continue your application</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
   )
 }
 
